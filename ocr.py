b37e87e96720eaa3e500479f771547fe507bf63b
fib_nm <bonolak@mail.ru>
2025-03-23 17:43:17 +0300

    Added language selection option

--- ocr.py
+++ ocr.py
@@ -15,30 +15,40 @@ def write_clipboard_text(text):
     subprocess.run(["wl-copy", "--type", "text/plain;charset=utf-8", text])
 
 def main():
-    print("Press Enter to extract text from the image in the clipboard (Ctrl-D to exit).")
+    languages = pytesseract.get_languages(config='')
+    print("Available languages: " + ", ".join(languages))
+    current_language = 'eng'
+
     while True:
+        print("Current language:", current_language)
+
         try:
-            input()
+            user_input = input()
         except EOFError:
             break
+        
+        if not user_input:
+            image = read_clipboard_image()
+            if image:
+                print("Image successfully pasted!")
 
-        image = read_clipboard_image()
-        if image:
-            print("Image successfully pasted!")
+                text = pytesseract.image_to_string(image, lang=current_language)
+                print("Text successfully extracted!")
 
-            text = pytesseract.image_to_string(image)
-            print("Text successfully extracted!")
+                write_clipboard_text(text)
+                print("Extracted text copied to the clipboard!")
 
-            write_clipboard_text(text)
-            print("Extracted text copied to the clipboard!")
-
-            terminal_width = shutil.get_terminal_size().columns
-            print("Extracted text:")
-            print("=" * terminal_width)
-            print(text)
-            print("=" * terminal_width)
+                terminal_width = shutil.get_terminal_size().columns
+                print("Extracted text:")
+                print("=" * terminal_width)
+                print(text)
+                print("=" * terminal_width)
+            else:
+                print("No image found in the clipboard!")
+        elif user_input in languages:
+            current_language = user_input
         else:
-            print("No image found in the clipboard!")
+            print("Unrecognized command. Enter available language, or press Enter to extract text from image.")
 
 if __name__ == "__main__":
     main()
